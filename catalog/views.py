2fa0ae63426ab16f9fc4239065e1a44e819ce54a
Maksim Netesov <MrEasters@mail.ru>
2024-07-18 11:09:51 +0300

    push project to github

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+    message = request.POST.get('message')
+    print(f'Имя: {name}, Тел.: {phone}\nСообщение: {message}')
+    return render(request, 'contacts.html')
