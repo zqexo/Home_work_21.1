ab3e25c9861be606404a88380159da4ba26148cf
Maksim Netesov <MrEasters@mail.ru>
2024-07-19 16:17:27 +0300

    edit admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,10 +4,11 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "is_active")
-    list_filter = ("is_active",)
-    search_fields = ("name",)
+    list_display = ("pk", "name", "price", "product_category")
+    list_filter = ("product_category",)
+    search_fields = ("name", "description")
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("pk", "name",)
