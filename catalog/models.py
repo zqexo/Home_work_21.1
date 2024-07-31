84d5d53673dbcd0c85f6b5a551d0a2c263f30938
Maksim Netesov <MrEasters@mail.ru>
2024-07-31 14:20:17 +0300

    add views_counter and migrate

--- catalog/models.py
+++ catalog/models.py
@@ -23,10 +23,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование")
-    description = models.TextField(verbose_name="Описание", **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name="Наименование", help_text="Введите название")
+    description = models.TextField(verbose_name="Описание", **NULLABLE, help_text="Введите описание")
     image_previews = models.ImageField(
-        upload_to="products/", verbose_name="Превью", **NULLABLE
+        upload_to="products/", verbose_name="Превью", **NULLABLE, help_text="Выберите фото"
     )
     product_category = models.ForeignKey(
         Category,
@@ -34,8 +34,9 @@ class Product(models.Model):
         related_name="products",
         **NULLABLE,
         verbose_name="Категория",
+        help_text="Укажите категорию",
     )
-    price = models.IntegerField(verbose_name="Цена")
+    price = models.IntegerField(verbose_name="Цена", help_text="Укажите цену",)
     created_at = models.DateTimeField(
         auto_now_add=True, verbose_name="Дата создания записи"
     )
@@ -43,6 +44,7 @@ class Product(models.Model):
         auto_now=True, verbose_name="Дата последнего изменения записи"
     )
 
+    views_counter = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
     is_active = models.BooleanField(default=True, verbose_name="В наличии")
 
     def __str__(self):
--- catalog/views.py
+++ catalog/views.py
@@ -16,42 +16,37 @@ def contacts(request):
     phone = request.POST.get("phone")
     message = request.POST.get("message")
     print(f"Имя: {name}, Тел.: {phone}\nСообщение: {message}")
-    return render(request, "contacts.html")
+    return render(request, "catalog/contacts.html")
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = "products_list.html"
     context_object_name = "products"
     extra_context = {"title": "Продукты"}
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = "products_detail.html"
     context_object_name = "product"
     extra_context = {"title": "Продукт"}
 
 
 def register(request):
-    return render(request, "register.html")
+    return render(request, "catalog/register.html")
 
 
 class ProductCreateView(CreateView):
     model = Product
-    template_name = "products_form.html"
     fields = "__all__"
     success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    template_name = "products_form.html"
     fields = "__all__"
     success_url = reverse_lazy("catalog:product_list")
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    template_name = "products_confirm_delete.html"
     success_url = reverse_lazy("catalog:product_list")
